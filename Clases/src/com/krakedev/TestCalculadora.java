fac7525c7f9463d8727a4e8bc2eac7969ead2dfc
Jose-Valdez7 <joxzsian@gmail.com>
2024-03-07 20:32:56 -0500

    agregado invocacion de las funciones en testCalculadora

--- Clases/src/com/krakedev/TestCalculadora.java
+++ Clases/src/com/krakedev/TestCalculadora.java
@@ -6,13 +6,23 @@ public class TestCalculadora {
 		 Calculadora calcu;
 		 int resultadoSuma;
 		 int resultadoResta;
+		 double resultadoMultiplicar;
+		 double resultadoDividir;
+		 double resultadoPromediar;
 		 
 		 calcu=new Calculadora();
 		 resultadoSuma=calcu.sumar(5, 8);
 		 resultadoResta=calcu.restar(10, 3);
+		 resultadoMultiplicar=calcu.multiplicar(10, 5);
+		 resultadoDividir=calcu.dividir(250, 10);
+		 resultadoPromediar=calcu.promediar(45, 5, 20);
+		 calcu.mostrarResultado();
 		 
 		 System.out.println("RESULTADO SUMA: "+resultadoSuma);
 		 System.out.println("RESULTADO RESTA: "+resultadoResta);
+		 System.out.println("RESULTADO MULTIPLICAR: "+resultadoMultiplicar);
+		 System.out.println("RESULTADO DIVIDIR: "+resultadoDividir);
+		 System.out.println("RESULTADO PROMEDIAR: "+resultadoPromediar);
 	}
 
 }
